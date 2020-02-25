21bbc0823394196f09a63b536d86f9c7199f3ca7
CodeVee <victorchike247Agmail.com>
2020-02-25 02:59:52 +0100

    Added Logging Service

---
+++ Bugz.Net/Bugz.Server.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+using Contracts;
+using LoggerService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bugz.Server.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options => 
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Bugz
+
+The is a project management system
+
+## Bugz.Net
+
+The Folder Containing the API written with C#.
+
+```c#
+cd Bugz.Net/Bugz.Server.API
+dotnet run
+```
