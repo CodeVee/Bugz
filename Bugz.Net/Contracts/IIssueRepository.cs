14dd3fb8c721a66da56554948e78172d33fe8ee4
CodeVee <victorchike247@gmail.com>
2020-03-01 23:59:09 +0100

    Extended issue contract

--- Bugz.Net/Contracts/IIssueRepository.cs
+++ Bugz.Net/Contracts/IIssueRepository.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Entities.Models;
 
 namespace Contracts
 {
     public interface IIssueRepository : IRepositoryBase<Issue>
     {
-        
+        Task<IEnumerable<Issue>> GetAllIssues();
+        Task<IEnumerable<Issue>> GetIssuesForProject(Guid projectId);
+        Task<Issue> GetIssue(Guid issueId);
+        void CreateIssue(Issue issue);
     }
 }
