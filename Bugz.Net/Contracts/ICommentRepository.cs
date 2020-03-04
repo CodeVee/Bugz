cd40d643c5d7d637dfcd2ed8c73195e9cc7be750
CodeVee <victorchike247@gmail.com>
2020-03-04 21:59:59 +0100

    Extended Comment Repository

--- Bugz.Net/Contracts/ICommentRepository.cs
+++ Bugz.Net/Contracts/ICommentRepository.cs
@@ -1,9 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Entities.Models;
 
 namespace Contracts
 {
     public interface ICommentRepository : IRepositoryBase<Comment>
     {
-         
+        Task<IEnumerable<Comment>> GetCommentsForTask(Guid taskId);
+        void CreateComment(Comment comment);
     }
 }
--- Bugz.Net/Repository/CommentRepository.cs
+++ Bugz.Net/Repository/CommentRepository.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Contracts;
 using Entities;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
@@ -8,6 +13,19 @@ namespace Repository
     {
         public CommentRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
+        }
+
+        public void CreateComment(Comment comment)
+        {
+            Create(comment);
+        }
+
+        public async Task<IEnumerable<Comment>> GetCommentsForTask(Guid taskId)
+        {
+            return await FindByCondition(comment => comment.TaskId.Equals(taskId))
+                        .Include(comment => comment.User)
+                        .OrderByDescending(comment => comment.Posted)
+                        .ToListAsync();
         }
     }
 }
